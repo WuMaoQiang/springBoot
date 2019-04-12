c1572beaa33409b26cac65cb6aa51aff707df394
wumaoqiang <wumaoqiang@vip.qq.com>
2019-04-12 10:41:43 +0800

    md 文档添加

--- src/main/java/com/cc/demo/DemoApplication.java
+++ src/main/java/com/cc/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.cc.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        System.out.print("ss");
     }
 
 }
