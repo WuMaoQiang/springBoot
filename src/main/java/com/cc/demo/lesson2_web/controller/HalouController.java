c8b3b510b164faf7a0a28b3a61b0fb171f07095e
wumaoqiang <wumaoqiang@vip.qq.com>
2019-04-18 17:41:49 +0800

    templates，tymeleaf

--- src/main/java/com/cc/demo/lesson2_web/controller/HalouController.java
+++ src/main/java/com/cc/demo/lesson2_web/controller/HalouController.java
@@ -1,12 +1,21 @@
 package com.cc.demo.lesson2_web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HalouController {
+    @ResponseBody
     @RequestMapping("/hello2")
     public String hello2() {
         return "hello2";
+    }
+
+//只有写成Controller 才会返回html页面
+    @RequestMapping("/success")
+    public String success() {
+        return "success";
     }
 }
