07ada07dc72795416cfe348bf94da5ef05e3cc43
wumaoqiang <wumaoqiang@vip.qq.com>
2019-05-24 17:20:56 +0800

    软引用 若引用

--- src/main/java/com/cc/demo/reference/Reference.java
+++ src/main/java/com/cc/demo/reference/Reference.java
@@ -1,23 +1,37 @@
 package com.cc.demo.reference;
 
 import com.cc.demo.lesson1.bean.Person;
-import sun.misc.GC;
-
-import java.lang.ref.SoftReference;
-import java.lang.ref.WeakReference;
-import java.util.HashMap;
 
 public class Reference {
 
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        String aa = "1230";
+        String bb = "1230";
+        System.out.println(aa == bb);
+        System.out.println(aa.equals(bb));
 
-        Person p = new Person();
-        WeakReference<Person> softReference = new WeakReference<Person>(p);
-        p=null;
-        System.gc();
-        Person person = softReference.get();
-        System.out.println(p + "..." + person);
+
+
+
+//        Person p = new Person();
+//        WeakReference<Person> softReference = new WeakReference<Person>(p);
+//        p=null;
+//        System.gc();
+//        Person person = softReference.get();
+//        System.out.println(p + "..." + person);
+
+
+//        ReferenceQueue<Object> referenceQueue = new ReferenceQueue<>();
+//
+//        PhantomReference<Person> personPhantomReference = new PhantomReference<>(p, referenceQueue);
+//        personPhantomReference.get();
+//        System.out.println(p + "..." + ".." + referenceQueue.poll());
+//
+//        p = null;
+//        System.gc();
+//        System.out.println(p + "..." + ".." + referenceQueue.poll());
+
 
     }
 }
